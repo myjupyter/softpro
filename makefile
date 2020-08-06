eaa7d735a776e8d78bd194f6880c323dab3821e3
Tkachuk Kirill <kirilltchkap@gmail.com>
2020-08-07 01:53:12 +0300

    Added Viper configurator, config.json, .gitingnore

--- makefile
+++ makefile
@@ -1,12 +1,8 @@
-run_docker:
-	sudo docker container start test happy_darwin
-	
-build:
-	go build ./src/main.go 
-	./main
+TARGET=service.out
+
+build: proto
+	go build -o $(TARGET) src/main.go 
 
 proto:
 	 protoc -I api/proto --go_out=plugins=grpc:api/subscription api/proto/subscription.proto
 
-stop:
-	sudo docker container stop test happy_darwin
--- src/main.go
+++ src/main.go
@@ -4,62 +4,88 @@ import (
     "net"
     "net/http"
     "softpro/work"
+    "flag"
     subs     "softpro/api/subscription"
     grpcserv "softpro/grpc"
     grpc     "google.golang.org/grpc"
     log      "github.com/sirupsen/logrus"
     ttool    "github.com/tarantool/go-tarantool"
+    viper    "github.com/spf13/viper"    
+)
+
+var (
+    Url      = "http://localhost:8000/api/v1/lines/"
+    DBAddr   = "127.0.0.1:3301"
+    HttpAddr = "127.0.0.1:8001"
+    GrpcAddr = "127.0.0.1:8002"
     
+    LogLevel = map[string]log.Level {
+        "debug"   : log.DebugLevel ,
+        "info"    : log.InfoLevel,
+        "warning" : log.WarnLevel,
+        "error"   : log.ErrorLevel,
+        "fatal"   : log.FatalLevel,
+    }
 )
 
-const (
-    Url = "http://localhost:8000/api/v1/lines/"
-    DBAddress = "127.0.0.1:3301"
-    Address = ":8081"
-)
-
-//TODO: передача аргументов в приложение:
-// 1. Адрес HTTP
-// 2. Адресс GRPC
-// 3. Секунды на каждый вид спорта
-// 4. Адрес хранилища
-// 5. Уровень логирования
 func main() {
-    log.SetLevel(log.DebugLevel)
+    // Reads flags 
+    name := flag.String("name", "config", "Configuration file name without extension")
+    dir := flag.String("dir", ".", "Configuration file directory")
+    
+    flag.Parse()
 
-    // TODO: настроить передачу времени
-    sports := []work.SportSubs{
-        work.SportSubs{"soccer",   3},
-        work.SportSubs{"football", 2},
-        work.SportSubs{"baseball", 4},
+    // Loads configs
+    viper.SetConfigName(*name)
+    viper.AddConfigPath(*dir)
+    if err := viper.ReadInConfig(); err != nil {
+        log.Fatal(err)
     }
 
-    conn, err := ttool.Connect(DBAddress, ttool.Opts{})
+    // Sets Logger Level
+    level, ok := LogLevel[viper.GetString("loglevel")]
+    if ok {
+        log.SetLevel(level)
+    } else {
+        log.Warning("Wrong logger level. Debug level is setted by default")
+        log.SetLevel(log.DebugLevel)
+    }
+
+    var sports []work.SportSubs
+    for sport, sec := range viper.GetStringMap("lineprovider.sports") {
+        sports = append(sports, work.SportSubs{Sport: sport, Seconds: int(sec.(float64))})
+    }
+
+    // Connection to the Data Storage
+    DBAddr = viper.GetString("datastorage.address") + ":" + viper.GetString("datastorage.port")
+    conn, err := ttool.Connect(DBAddr, ttool.Opts{
+        User: viper.GetString("datastorage.login"),
+        Pass: viper.GetString("datastorage.password"),
+    })
     if err != nil {
-        log.WithFields(log.Fields{
-            "who" : "Data Storage",
-        }).Fatal(err)
+        log.WithFields(log.Fields{"what" : "Data Storage",}).Fatal(err)
         return
     }
     defer conn.Close()
-    log.Debug("Connected to Data Storage")
 
     var pool work.WorkerPool
     pool.Start(conn, Url, sports...)
-    log.Debug("Pool has been run")
-
     
-    log.Debug("HTTP server listens")
+    // Sets handler for /ready
     http.HandleFunc("/ready", pool.ReadyHandler)
-    go http.ListenAndServe(Address, nil) 
+    
+    HttpAddr = viper.GetString("http.address") + ":" + viper.GetString("http.port")
+    go http.ListenAndServe(HttpAddr, nil) 
 
+
+    // Waits for the first line synchronization
     if pool.CheckWorkersSync() {
-        log.Debug("GRPC server listens")
         s := grpc.NewServer()
         srv := &grpcserv.GRPCServer{Conn: conn}
 
         subs.RegisterSubscribtionServer(s, srv)
-        l, err := net.Listen("tcp", ":8080")
+        GrpcAddr = viper.GetString("grpc.address") + ":" + viper.GetString("grpc.port")
+        l, err := net.Listen("tcp", GrpcAddr)
         if err != nil {
             log.Fatal(err)
         }
